0facc7c348b560ef806ee64428007e2466794a19
Rayan <rayan.mazri@epitech.eu>
2023-05-09 19:43:38 +0200

    My portfolio

---
+++ script.js
@@ -0,0 +1,31 @@
+button {
+}
+font-family: "Torus Notched";
+body {
+overflow-x: hidden;
+max-width: 1920px;
+}
+/*--HEADER--*/
+header {
+}
+height: 120px;
+width: 100%;
+top: 0;
+left: 0;
+position: absolute;
+header .container {
+}
+width: 1500px;
+height: 100%;
+margin: 0 auto;
+display: flex;
+align-items: center;
+flex-direction: row;
+justify-content: space-between;
+header .container ul {
+}
+display: flex;
+header .container ul a {
+font-size: 1.2em;
+font-weight: 500;
+margin: 0 30px; color:#66aefc;
